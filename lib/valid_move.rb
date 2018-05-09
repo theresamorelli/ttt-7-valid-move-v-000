b0f22484e1afc44dd75484ec322d41a224d60afc
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-09 20:35:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if !position_taken?(board, index) && 0 <= index <= 8
+  if !position_taken?(board, index) && (0..8).include(index)
     true
   else
     false
